3fd3f7dfd512850e69717c6d3a8f5db2d748ccfa
Pedro <ipeteperez@hotmail.com>
2023-11-19 14:28:31 -0800

    removed unused imports

--- src/components/common/left_sidebar/LeftSidebar.js
+++ src/components/common/left_sidebar/LeftSidebar.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+//import axios from "axios";
+//import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./leftsidebar.css";
 import leftSideImage from "../../../assets/images/aboutImage.png"
--- src/pages/post_page/PostPage.js
+++ src/pages/post_page/PostPage.js
@@ -1,6 +1,6 @@
 // import React from 'react'
 import SinglePost from "../../components/content/singlePost/SinglePost.js"
-import Sidebar from "../../components/common/sidebar/Sidebar.js"
+//import Sidebar from "../../components/common/sidebar/Sidebar.js"
 import "./postpage.css"
 
 
