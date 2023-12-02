5dc31e71fcb5373511b6c31eafee15a00db52526
Pedro <ipeteperez@hotmail.com>
2023-12-02 08:34:48 -0800

    action fix

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./header.css"
-import hdrImage1 from "../../../assets/images/header2.jpg";
+//import hdrImage1 from "../../../assets/images/header2.jpg";
 
 export default function Header() {
   return (
