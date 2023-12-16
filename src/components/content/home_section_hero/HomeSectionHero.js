7824b86d22da3749dd0d0c1775e75e3da11eed69
Pedro <ipeteperez@hotmail.com>
2023-12-15 19:58:16 -0800

    updates

--- src/components/content/home_section_hero/HomeSectionHero.js
+++ src/components/content/home_section_hero/HomeSectionHero.js
@@ -23,33 +23,8 @@ export default function HomeSectionHero() {
                     Website Development Steps &raquo;
                 </button>
         
-        </div>
+            </div>
         </section>
     )
-        {/*<div className="homesectionhero-two-column">
-            <div className="homesectionhero-right-column">
-                <div className="homesectionhero-right-column-image">
-                <img className = "image1" src={image1} alt="personal website" />
-                </div>
-            </div>
-    
-            <div className="homesectionhero-left-column">
-                <div className="homesectionhero-left-column-content">
-                <h1 className="homesectionhero-left-column-tagline">You should have a personal website!</h1>
-                <p className="homesectionhero-left-column-text">
-                    A personal website tell the world who you are.  
-                    At <b>newWebDev</b>, we will show people how to create websites to showcase their expriences, creativity, and talents.
-                </p>
-
-                <button className="homesectionhero-left-column-call-to-action-button">
-                    <Link className="link" to="/contact">
-                    Website Development Steps
-                    </Link>
-                </button>
-                </div>
-            </div>
-    
-        </div>
-    */}
 
   }
