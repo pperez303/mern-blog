7d3d1cd67a60439cbb4c21b3e4051f738c8dc185
Pedro <ipeteperez@hotmail.com>
2023-12-17 17:15:32 -0800

    testing media aboutpage

--- src/pages/contact_page/ContactPage.js
+++ src/pages/contact_page/ContactPage.js
@@ -35,29 +35,6 @@ function Contact () {
                 </div>
             </div>
         </div>
-
-{/*<main className="contactArea">
-            <div className="contactInfo">
-                <header className="contactHeader">
-                    <img className = "contactImg" src={neo} alt="" />
-                    <div className="contactName">
-                        <h3>Peter Perez</h3>
-                        <p>Technical Project Manager</p>
-                    </div>
-                </header>
-                <p className="contactPhone">
-                    phone: 1-702-468-8204
-                </p>
-                <p className="contactEmail">
-                    email: peteperez.lv@gmail.com
-                </p>
-            </div>
-            <div className="contactSidebar">
-                 <Sidebar/>
-            </div>
-           
-        </main>
-    */}
     </>
     )
 }  
